33a421465ffc3af40eb6c8232aa4d75c95622ecd
Andrey Marchenko <tom910ru@gmail.com>
2020-10-28 13:58:22 +0300

    Check version updatw

--- pages/index.js
+++ pages/index.js
@@ -40,7 +40,7 @@ const Index = () => (
         Notice that the image is responsive. As you adjust your browser width, a
         different sized image is loaded.
       </p>
-      <Image alt="Vercel logo" src={getImg('logo')} width={getRandomWidth()} height={1000} />
+      <Image alt="Vercel logo 2" src={getImg('logo')} width={getRandomWidth()} height={1000} />
       <hr className={styles.hr} />
       <p>
         The following is an example of a reference to an external image at{' '}
